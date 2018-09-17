5c54872feb4b5f8b320c7173836b220274c4d1c4
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2018-09-17 16:51:13 +0200

    update for geant3/4

--- cmake/cmake-configure-o2.sh
+++ cmake/cmake-configure-o2.sh
@@ -9,7 +9,7 @@ source=${2:-"/Users/laurent/alice/o2-dev/O2"}
 install=${3:-"/Users/laurent/alice/sw/"}
 dest=${4:-"/Users/laurent/alice/sw/"}
 label=${5:-"$version"}
-version=${6:-"latest-run3-o2"}
+version=${6:-"latest-dpl-mch-test-o2-ninja"}
 
 case $OSTYPE in
 linux*)
@@ -32,12 +32,11 @@ cmake $source \
 -DFairRoot_DIR=$install/$platform/FairRoot/$version/ \
 -DBOOST_ROOT=$install/$platform/boost/$version/ \
 -DROOTSYS=$install/$platform/ROOT/$version/ \
--DGeant3_DIR="$install/$platform/GEANT3/$version/$g3lib" \
--DGeant4_DIR="$install/$platform/GEANT4/$version/lib" \
--DGEANT4_VMC_DIR="$install/$platform/GEANT4_VMC/$version/lib" \
--DVGM_DIR="$install/$platform/vgm/$version/lib" \
+-DGeant3_DIR="$install/$platform/GEANT3/$version/$g3lib/Geant3-2.5.0" \
+-DGeant4_DIR="$install/$platform/GEANT4/$version/lib/Geant4-10.3.3" \
+-DGeant4VMC_DIR="$install/$platform/GEANT4_VMC/$version/lib/Geant4VMC-3.5.0" \
+-DVGM_DIR="$install/$platform/vgm/$version/lib/VGM-4.4.0" \
 -DVc_DIR="$install/$platform/Vc/$version/lib/cmake/Vc" \
--DCMAKE_PREFIX_PATH="$install/$platform/vgm/$version/lib/VGM-4.4.0;$install/$platform/GEANT4_VMC/$version/lib/Geant4VMC-3.5.0;$install/$platform/GEANT4/$version/lib/Geant4-10.3.3;$install/$platform/GEANT3/$version/$g3lib/Geant3-2.5.0;$install/$platform/ROOT/$version;$install/$platform/Vc/$version;$install/$platform/FairRoot/$version" \
 -DCMAKE_LIBRARY_PATH="$install/$platform/vgm/$version/lib;$install/$platform/GEANT4_VMC/$version/lib;$install/$platform/GEANT4/$version/lib;$install/$platform/GEANT3/$version/$g3lib;$install/$platform/ROOT/$version" \
 -DCMAKE_MODULE_PATH="$source/cmake/modules;$install/$platform/FairRoot/$version/share/fairbase/cmake/modules" \
 -DPythia6_LIBRARY_DIR=$install/$platform/pythia6/$version/lib \
@@ -55,15 +54,16 @@ cmake $source \
 -Dbenchmark_DIR=$install/$platform/googlebenchmark/$version/lib/cmake/benchmark \
 -DMS_GSL_INCLUDE_DIR=$install/$platform/ms_gsl/$version/include \
 -DALITPCCOMMON_DIR=$install/$platform/AliTPCCommon/$version \
--DBUILD_TESTING=ON \
+-DBUILD_TESTING=OFF \
 -DCMAKE_BUILD_TYPE=Debug \
 -DCMAKE_CXX_STANDARD=14 \
 -G "$generator"
 EOF
 )
 
-eval "$cmd"
+#export LD_LIBRARY_PATH="$install:$platform/lib"
+#eval "$cmd"
 
 # uncomment the line below to generate a piece of JSON suitable for insertion in VSCode settings.json file
 # for CMake Tools extension.
-#echo $cmd | tr " " "\n" | grep "^-D" | awk '{ print "\"" $1"\","}' | sed s/-D//g | sed s/=/\":\"/g | sed s_\/\/_\/_g | sed s/\"\"/\"/g
+echo $cmd | tr " " "\n" | grep "^-D" | awk '{ print "\"" $1"\","}' | sed s/-D//g | sed s/=/\":\"/g | sed s_\/\/_\/_g | sed s/\"\"/\"/g
