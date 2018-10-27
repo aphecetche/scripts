eaf4f070a43fd94dbb0698d7cdc3fb6b60ae9482
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2018-10-27 19:45:36 +0200

    Produce a new format (compatible with goaf)

--- alice/run2/saf/getfilelists/filterfilelist.sh
+++ alice/run2/saf/getfilelists/filterfilelist.sh
@@ -3,13 +3,12 @@
 for filelist in $(ls nansaf*.files.list)
 do
 
-#period=$1
-#pass=$2
-#datatype=${3:AliESD}
-#cat $filelist | cut -d ' ' -f 5 | grep "$pass" | grep "$datatype" | grep "$period" | sed s_/data/alice_/alice_g | awk '{printf "xrdfs nansafmaster2 rm %s\n",$1;} '
+period=$1
+pass=$2
+datatype=${3:AliESD}
+cat $filelist | cut -d ' ' -f 4,5 | grep "$pass" | grep "$datatype" | grep "$period" | sed s_/data/alice_/alice_g | awk '{printf "xrdfs " $2 " rm %s\n",$1;} ' | sed s/SAF-//g
 
-search="OCDB"
-
-cat $filelist | cut -d ' ' -f 4,5 | grep $search | sed s_/data/alice_/alice_g | awk '{printf "xrdfs " $2 " rm %s\n",$1;} ' | sed s/SAF-//g
+#search="OCDB"
+#cat $filelist | cut -d ' ' -f 4,5 | grep $search | sed s_/data/alice_/alice_g | awk '{printf "xrdfs " $2 " rm %s\n",$1;} ' | sed s/SAF-//g
 
 done
--- alice/run2/saf/getfilelists/getfilelists.sh
+++ alice/run2/saf/getfilelists/getfilelists.sh
@@ -7,9 +7,9 @@ legacy=${1:-no}
 echo "#!/bin/sh" > $output
 
 if [[ "$legacy" == "yes" ]]; then
-  find /data -name *.root -type f -exec stat -c "%s %Y %n" {} \; | awk '{print strftime("%a %d.%m.%Y %H:%M:%S ",$2)  $1 " " $3 " SAF-" ENVIRON["machine"] }' > $output
+  find /data -name *.root -type f -exec stat -Lc "%s %Y %n" {} \; | awk '{print strftime("%a %d.%m.%Y %H:%M:%S ",$2)  $1 " " $3 " SAF-" ENVIRON["machine"] }' > $output
 else
-  find /data -name *.root -type f -exec stat -c "%s %Y %X %n" {} \; | awk '{print $2 " " $3 " " $1 " " $4 " SAF-" ENVIRON["machine"] }' > $output
+  find /data -name *.root -type f -exec stat -Lc "%s %Y %X %n" {} \; | awk '{print $2 " " $3 " " $1 " " $4 " SAF-" ENVIRON["machine"] }' > $output
 fi
 
 
