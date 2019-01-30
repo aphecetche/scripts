51808d939079b5e34bbd5e42b2b20b91def43e3d
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-01-30 15:28:44 +0100

    Add .inl alternates

--- alice/run3/generate-projections-json.go
+++ alice/run3/generate-projections-json.go
@@ -7,16 +7,19 @@ import (
 )
 
 func alternate(w io.Writer, base, include string) {
-	fmt.Fprintf(w, "\"%s/include/%s/*.h\" : { \"alternate\":\"%s/src/{}.cxx\" },\n",
-		base, include, base)
-	fmt.Fprintf(w, "\"%s/src/*.cxx\" : { \"alternate\":\"%s/include/%s/{}.h\" },\n", base, base, include)
+	fmt.Fprintf(w, "\"%s/include/%s/*.h\" : { \"alternate\": [ \"%s/include/%s/{}.inl\", \"%s/src/{}.cxx\" ] },\n",
+		base, include, base, include, base)
+	fmt.Fprintf(w, "\"%s/src/*.cxx\" : { \"alternate\":\"%s/include/%s/{}.h\" },\n",
+		base, base, include)
+	fmt.Fprintf(w, "\"%s/include/%s/*.inl\" : { \"alternate\":\"%s/include/%s/{}.h\" },\n",
+		base, include, base, include)
 }
 
 func generate(w io.Writer) {
 	fmt.Fprintf(w, "{\n")
 	generateAlternates(w)
 
-	clangTidyBinary := "$HOME/alice/sw/osx_x86-64/latest/o2codechecker/bin/O2codecheck"
+	clangTidyBinary := "/Users/laurent/alice/sw/osx_x86-64/o2codechecker/latest/bin/O2codecheck"
 
 	fmt.Fprintf(w, "\"*.cxx\" : { \"clang-tidy-binary\":\"%s\" }\n",
 		clangTidyBinary)
@@ -27,6 +30,7 @@ func generateAlternates(w io.Writer) {
 	alternate(w, "Detectors/MUON/MCH/Simulation", "MCHSimulation")
 	alternate(w, "Detectors/MUON/MCH/Base", "MCHBase")
 	alternate(w, "Detectors/MUON/MCH/Mapping/SegContour", "MCHMappingSegContour")
+	alternate(w, "Detectors/MUON/MCH/Mapping/Interface", "MCHMappingInterface")
 	alternate(w, "Detectors/MUON/MID/Simulation", "MIDSimulation")
 	alternate(w, "Detectors/MUON/MID/Base", "MIDBase")
 	alternate(w, "Detectors/MUON/MID/Clustering", "MIDClustering")
