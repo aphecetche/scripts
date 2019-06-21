b3bd92f163ead113ab84d470f6b3577f3c350887
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-06-21 15:04:41 +0200

    update vmware fusion backup script

--- vmware/vmware-backup-vms.sh
+++ vmware/vmware-backup-vms.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-
 DESTDIR="/Volumes/VMs Backup 1 To"
 
 [[ -e $DESTDIR ]] || exit
 
-for vm in "$(find "/Users/laurent/Documents/Virtual Machines.localized" -name *.vmwarevm)"; do
-   printf "Backup of %s\\n" "$vm"
-    "/Applications/VMware Fusion.app/Contents/Library/vmrun" -T fusion stop "$vm" 2>&1 > /dev/null
+function backup() {
+  local vm=$1
+  printf "Backup of %s to $DESTDIR\\n" "$vm"
+  "/Applications/VMware Fusion.app/Contents/Library/vmrun" -T fusion stop "$vm" 2>&1 >/dev/null
   rsync -av "$vm" "$DESTDIR/"
-done
+}
+
+find /Users/laurent/Virtual\ Machines.localized -name "*.vmwarevm" | while read file; do backup "$file"; done
+
