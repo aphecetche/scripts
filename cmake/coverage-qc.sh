6301b2e0e5b9302afc2317f2703bd5b8359d973a
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-09-24 11:18:34 +0200

    wip

---
+++ cmake/coverage-qc.sh
@@ -0,0 +1,6 @@
+# example of upload to codecov.io
+# the commit hash must be the full one (not the abbrev)
+
+
+$HOME/scripts/cmake/codecov-uploader -f coverage.info -R /Users/laurent/alice/cmake/QualityControl -C f8f89bc9c4917f35440998077cdd7d416589648a  -B v0.18.1 -t eeb7ec04-34f7-4140-8f2d-8687c59ba0a5
+
