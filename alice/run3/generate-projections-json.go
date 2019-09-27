01302085e1d9bff62339a469e1c4b95cebf8d449
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-09-27 16:30:33 +0200

    Add cycling source->test->header

--- alice/run3/generate-projections-json.go
+++ alice/run3/generate-projections-json.go
@@ -7,12 +7,19 @@ import (
 )
 
 func alternate(w io.Writer, base, include string) {
-	fmt.Fprintf(w, "\"%s/include/%s/*.h\" : { \"alternate\": [ \"%s/include/%s/{}.inl\", \"%s/src/{}.cxx\" ] },\n",
-		base, include, base, include, base)
-	fmt.Fprintf(w, "\"%s/src/*.cxx\" : { \"alternate\":\"%s/include/%s/{}.h\" },\n",
+	// Implementation file -> test file or include file (first found)
+	fmt.Fprintf(w, "\"%s/src/*.cxx\" : { \"type\": \"source\", \"alternate\": [ \"%s/test/test{}.cxx\", \"%s/include/%s/{}.h\" ] },\n",
+		base, base, base, include)
+	// include -> implementation or test (first found)
+	fmt.Fprintf(w, "\"%s/include/%s/*.h\" : { \"type\": \"header\", \"alternate\": [ \"%s/src/{}.cxx\", \"%s/test/test{}.cxx\" ] },\n",
+		base, include, base, base)
+	// test -> include or implementation (first found)
+	fmt.Fprintf(w, "\"%s/test/test*.cxx\" : { \"type\":\"test\", \"alternate\": [ \"%s/include/%s/{}.h\"] },\n",
 		base, base, include)
-	fmt.Fprintf(w, "\"%s/include/%s/*.inl\" : { \"alternate\":\"%s/include/%s/{}.h\" },\n",
-		base, include, base, include)
+
+	// inl files : no alternate, but of type "inl"
+	fmt.Fprintf(w, "\"%s/include/%s/*.inl\" : { \"type\" : \"inl\" },\n",
+		base, include)
 }
 
 func generate(w io.Writer) {
@@ -27,16 +34,20 @@ func generate(w io.Writer) {
 }
 
 func generateAlternates(w io.Writer) {
+	alternate(w, "Detectors/MUON/MCH/Raw", "MCHRaw")
 	alternate(w, "Detectors/MUON/MCH/Simulation", "MCHSimulation")
 	alternate(w, "Detectors/MUON/MCH/Base", "MCHBase")
 	alternate(w, "Detectors/MUON/MCH/Mapping/SegContour", "MCHMappingSegContour")
 	alternate(w, "Detectors/MUON/MCH/Mapping/Interface", "MCHMappingInterface")
+	//
 	alternate(w, "Detectors/MUON/MID/Simulation", "MIDSimulation")
 	alternate(w, "Detectors/MUON/MID/Base", "MIDBase")
 	alternate(w, "Detectors/MUON/MID/Clustering", "MIDClustering")
 	alternate(w, "Detectors/MUON/MID/Tracking", "MIDTracking")
 	alternate(w, "Detectors/MUON/MID/TestingSimTools", "MIDTestingSimTools")
+	//
 	alternate(w, "Common/Field", "Field")
+	//
 	alternate(w, "Detectors/TPC/simulation", "TPCsimulation")
 	alternate(w, "Detectors/TPC/base", "TPCbase")
 }
