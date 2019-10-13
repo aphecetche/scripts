ad178039a9a5bf235d374e73cb2c36b30169b054
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-10-13 19:16:25 +0200

    change alienv-build

--- alice/run3/alienv-build.sh
+++ alice/run3/alienv-build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-what=${1:-O2}
+# what=${1:-O2}
+#
+# cd $HOME/alice/sw/BUILD/$what-latest/$what 
+#
+# alienv --work-dir $HOME/alice/sw setenv $what/latest,CMake/latest -c cmake --build . -- install
 
-cd $HOME/alice/sw/BUILD/$what-latest/$what 
+cd $ALIBUILD_WORK_DIR/BUILD/O2-latest/O2
+ninja
 
-alienv --work-dir $HOME/alice/sw setenv $what/latest,CMake/latest -c cmake --build . -- install
