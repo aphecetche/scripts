6f70f3e7f71804667aa6ad384da155b857e59668
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2024-02-01 17:27:26 +0100

    km3net

--- ninja/ninja-log-2-speedscope.py
+++ ninja/ninja-log-2-speedscope.py
@@ -3,6 +3,7 @@
 import sys
 import re
 
+
 def read_logfile(log):
     header = log.readline()
     m = re.search(r'^# ninja log v(\d+)\n$', header)
@@ -16,8 +17,9 @@ def read_logfile(log):
     targets = {}
     last_end_seen = 0
     for line in log:
-        start, end, _, name, cmdhash = line.strip().split('\t') # Ignore restat.
-        if re.match("CMakeFiles",name):
+        # Ignore restat.
+        start, end, _, name, cmdhash = line.strip().split('\t')
+        if re.match("CMakeFiles", name):
             continue
         if int(end) < last_end_seen:
             # An earlier time stamp means that this step is the first in a new
@@ -31,83 +33,64 @@ def read_logfile(log):
             t = {
                 'start': int(start),
                 'end': int(end),
-                'name':name,
-                'group':'-',
+                'name': name,
+                'group': '-',
                 'duration': int((int(end)-int(start))/1000)
             }
-            targets[cmdhash]=t
+            targets[cmdhash] = t
     return sorted(targets.values(), key=lambda x: x['duration'], reverse=True)
 
-def group_targets(targets):
-    groups = [ "^GPU","^Steer","^Detectors","^Framework",
-              "^Analysis",
-              "^Framework/Core",
-              "^Framework/TestWorkflows",
-              "^Analysis",
-              "^Detectors/MUON",
-              "^Detectors/MUON/MCH",
-              "^Detectors/MUON/MCH/Simulation",
-              "^Detectors/MUON/MCH/Tracking",
-              "^Detectors/MUON/MCH/PreClustering",
-              "^Detectors/MUON/MCH/Mapping",
-              "^Detectors/MUON/MCH/Raw",
-              "^Detectors/MUON/MCH/Raw/ElecMap",
-              "^Detectors/MUON/MCH/Raw/Encoder",
-              "^Detectors/MUON/MCH/Raw/Decoder",
-              "^Detectors/MUON/MID",
-              "^Detectors/ITSMFT",
-              "^Detectors/ITSMFT/ITS",
-              "^Detectors/ITSMFT/MFT",
-              "^Detectors/ITSMFT/common",
-              "^Detectors/TRD",
-              "^Detectors/FIT",
-              "^Detectors/TOF",
-              "^Detectors/GlobalTracking",
-              "^Detectors/TPC",
-              "^Detectors/PHOS",
-              "^Detectors/EMCAL",
-              "^Detectors/Raw",
-              "^Detectors/CPV",
-              "^Detectors/Base",
-              "^Detectors/HMPID",
-              "^Detectors/gconfig",
-              "^Detectors/ZDC",
-              "^CCDB",
-              "^DataFormats",
-              "^run",
-              "^Generators",
-              "^Utilities",
-              "^Utilities/Merger",
-              "^Utilities/DataFlow",
-              "^Common",
-              "^Common/Field",
-              "^Common/MathUtils",
-              "^Common/Utils",
-              "^Common/SimConfig",
-              "^Examples",
-              "^stage"
 
-                 ]
+def group_targets(targets):
+    groups = [
+        "^examples",
+        "^examples/JDynamics",
+        "^examples/JReconstruction",
+        "^examples/JTrigger",
+        "^software",
+        "^software/JAAnet",
+        "^software/JAcoustics",
+        "^software/JCalibrate",
+        "^software/JCompass",
+        "^software/JDB",
+        "^software/JDataFilter",
+        "^software/JDetector",
+        "^software/JDynamics",
+        "^software/JGizmo",
+        "^software/JMonitor",
+        "^software/JPhysics",
+        "^software/JReconstruction",
+        "^software/JRunAnalyzer",
+        "^software/JDataQuality",
+        "^software/AcousticDataFilter",
+        "^software/JSirene",
+        "^software/JSupernova",
+        "^software/JTimeslice",
+        "^software/JTrigger",
+        "^software/JDataWriter",
+        "^tests"
+    ]
     for t in targets:
-      group = "-"
-      name = t['name']
-      for g in groups:
-        if re.search(g,name):
-            group = g.replace("/",";")
-            group = group.replace("^","")
-      if re.search("G__",name):
-          group="dict"
-      if re.search("workflow",name):
-          group = "workflow;{}".format(group)
-      if re.search("test",name):
-          group = "test;{}".format(group)
-      t['group'] = group
+        group = "-"
+        name = t['name']
+        for g in groups:
+            if re.search(g, name):
+                group = g.replace("/", ";")
+                group = group.replace("^", "")
+        if re.search("G__", name):
+            group = "dict"
+        if re.search("^software/.*Data.*", name):
+            group = "software;DAQ"
+        if re.search("test", name):
+            group = "test;{}".format(group)
+        t['group'] = group
     return targets
 
-with open(sys.argv[1], 'r') as log: 
+
+with open(sys.argv[1], 'r') as log:
     targets = read_logfile(log)
 
 targets = group_targets(targets)
 
 for t in targets:
-    print("{};{} {}".format(t['group'],t['name'],t['duration']))
+    print("{};{} {}".format(t['group'], t['name'], t['duration']))
